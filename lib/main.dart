bfc0228880536f719c7f21edf28d4f824db5985e
Max Blennemann <80426654+23maxb@users.noreply.github.com>
2026-05-19 17:45:02 -0700

    fixed curve types

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ enum AnimationType { rotate, scale, slide, fade }
 
 enum IconType { rocket, heart, gear, star }
 
+enum CurveType { linear, easeIn, easeOut, bounce, elastic }
+
 class AnimationLabApp extends StatelessWidget {
   const AnimationLabApp({super.key});
 
@@ -31,32 +33,20 @@ class _AnimationLabPageState extends State<AnimationLabPage>
   late AnimationController _controller;
   AnimationType _selectedType = AnimationType.rotate;
   IconType _selectedIcon = IconType.rocket;
+  CurveType _selectedCurve = CurveType.linear; // Default curve
   String _currentStatus = "READY";
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 2),
+      duration: const Duration(milliseconds: 800),
       vsync: this,
     );
 
     _controller.addStatusListener((status) {
       setState(() {
-        switch (status) {
-          case AnimationStatus.forward:
-            _currentStatus = "RUNNING FORWARD";
-            break;
-          case AnimationStatus.reverse:
-            _currentStatus = "RUNNING REVERSE";
-            break;
-          case AnimationStatus.completed:
-            _currentStatus = "COMPLETED";
-            break;
-          case AnimationStatus.dismissed:
-            _currentStatus = "READY";
-            break;
-        }
+        _currentStatus = status.name.toUpperCase();
       });
     });
   }
@@ -67,37 +57,34 @@ class _AnimationLabPageState extends State<AnimationLabPage>
     super.dispose();
   }
 
-  void _showAnimationSheet(BuildContext context) {
-    showCupertinoModalPopup<void>(
-      context: context,
-      builder: (BuildContext context) => CupertinoActionSheet(
-        title: const Text('Select Animation'),
-        actions: AnimationType.values.map((type) {
-          return CupertinoActionSheetAction(
-            onPressed: () {
-              setState(() => _selectedType = type);
-              _controller.reset();
-              Navigator.pop(context);
-            },
-            child: Text(type.name.toUpperCase()),
-          );
-        }).toList(),
-      ),
-    );
+  Curve _getActualCurve() {
+    switch (_selectedCurve) {
+      case CurveType.linear:
+        return Curves.linear;
+      case CurveType.easeIn:
+        return Curves.easeIn;
+      case CurveType.easeOut:
+        return Curves.easeOut;
+      case CurveType.bounce:
+        return Curves.bounceOut;
+      case CurveType.elastic:
+        return Curves.elasticOut;
+    }
   }
 
-  void _showIconSheet(BuildContext context) {
+  void _showSheet<T>(String title, List<T> values, Function(T) onSelect) {
     showCupertinoModalPopup<void>(
       context: context,
-      builder: (BuildContext context) => CupertinoActionSheet(
-        title: const Text('Select Icon'),
-        actions: IconType.values.map((type) {
+      builder: (context) => CupertinoActionSheet(
+        title: Text(title),
+        actions: values.map((value) {
+          final String name = value.toString().split('.').last.toUpperCase();
           return CupertinoActionSheetAction(
             onPressed: () {
-              setState(() => _selectedIcon = type);
+              onSelect(value);
               Navigator.pop(context);
             },
-            child: Text(type.name.toUpperCase()),
+            child: Text(name),
           );
         }).toList(),
       ),
@@ -114,156 +101,174 @@ class _AnimationLabPageState extends State<AnimationLabPage>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              alignment: WrapAlignment.center,
               children: [
-                CupertinoButton(
-                  color: CupertinoColors.systemGroupedBackground,
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  onPressed: () => _showAnimationSheet(context),
-                  child: Text(
-                    _selectedType.name.toUpperCase(),
-                    style: const TextStyle(
-                      color: CupertinoColors.activeBlue,
-                      fontSize: 14,
-                    ),
+                _buildPickerButton(
+                  _selectedType.name,
+                  CupertinoColors.activeBlue,
+                  () => _showSheet(
+                    "Animation",
+                    AnimationType.values,
+                    (v) => setState(() => _selectedType = v),
                   ),
                 ),
-                const SizedBox(width: 10),
-                CupertinoButton(
-                  color: CupertinoColors.systemGroupedBackground,
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  onPressed: () => _showIconSheet(context),
-                  child: Text(
-                    _selectedIcon.name.toUpperCase(),
-                    style: const TextStyle(
-                      color: CupertinoColors.activeOrange,
-                      fontSize: 14,
-                    ),
+                _buildPickerButton(
+                  _selectedIcon.name,
+                  CupertinoColors.activeOrange,
+                  () => _showSheet(
+                    "Icon",
+                    IconType.values,
+                    (v) => setState(() => _selectedIcon = v),
+                  ),
+                ),
+                _buildPickerButton(
+                  _selectedCurve.name,
+                  CupertinoColors.systemPurple,
+                  () => _showSheet(
+                    "Curve",
+                    CurveType.values,
+                    (v) => setState(() => _selectedCurve = v),
                   ),
                 ),
               ],
             ),
 
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-              decoration: BoxDecoration(
-                color: CupertinoColors.activeGreen.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Text(
-                "STATUS: $_currentStatus",
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: CupertinoColors.activeGreen,
-                  fontSize: 12,
-                ),
-              ),
-            ),
+            _buildStatusIndicator(),
+
             SizedBox(
               height: 200,
               child: AnimatedBuilder(
                 animation: _controller,
                 builder: (context, child) {
-                  return _buildAnimatedWidget();
+                  return _buildAnimatedWidget(
+                    _getActualCurve().transform(_controller.value),
+                  );
                 },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CupertinoButton.filled(
-                          onPressed: () => _controller.forward(),
-                          child: const Text('Start'),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: CupertinoButton(
-                          color: CupertinoColors.systemRed,
-                          onPressed: () => _controller.stop(),
-                          child: const Text('Stop'),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CupertinoButton(
-                          color: CupertinoColors.systemIndigo,
-                          onPressed: () => _controller.reverse(),
-                          child: const Text('Reverse'),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: CupertinoButton(
-                          color: CupertinoColors.systemGrey,
-                          onPressed: () => _controller.reset(),
-                          child: const Text('Reset'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+
+            _buildPlaybackControls(),
           ],
         ),
       ),
     );
   }
 
-  Widget _getIcon() {
-    IconData data;
-    Color color;
-    switch (_selectedIcon) {
-      case IconType.rocket:
-        data = CupertinoIcons.rocket_fill;
-        color = CupertinoColors.activeBlue;
-        break;
-      case IconType.heart:
-        data = CupertinoIcons.heart_fill;
-        color = CupertinoColors.systemRed;
-        break;
-      case IconType.gear:
-        data = CupertinoIcons.settings_solid;
-        color = CupertinoColors.systemGrey;
-        break;
-      case IconType.star:
-        data = CupertinoIcons.star_fill;
-        color = CupertinoColors.systemYellow;
-        break;
-    }
-    return Icon(data, size: 100, color: color);
+  Widget _buildPickerButton(String label, Color color, VoidCallback onPressed) {
+    return CupertinoButton(
+      color: CupertinoColors.systemGroupedBackground,
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      onPressed: onPressed,
+      child: Text(
+        label.toUpperCase(),
+        style: TextStyle(color: color, fontSize: 12),
+      ),
+    );
   }
 
-  Widget _buildAnimatedWidget() {
+  Widget _buildStatusIndicator() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: CupertinoColors.activeGreen.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Text(
+        "STATUS: $_currentStatus",
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: CupertinoColors.activeGreen,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedWidget(double animValue) {
     final activeIcon = _getIcon();
     switch (_selectedType) {
       case AnimationType.rotate:
         return Transform.rotate(
-          angle: _controller.value * 2 * math.pi,
+          angle: animValue * 2 * math.pi,
           child: activeIcon,
         );
       case AnimationType.scale:
         return Transform.scale(
-          scale: 0.5 + (_controller.value * 1.0),
+          scale: 0.5 + (animValue * 1.0),
           child: activeIcon,
         );
       case AnimationType.slide:
         return FractionalTranslation(
-          translation: Offset(0, -_controller.value),
+          translation: Offset(0, -animValue),
           child: activeIcon,
         );
       case AnimationType.fade:
-        return Opacity(opacity: _controller.value, child: activeIcon);
+        return Opacity(opacity: animValue.clamp(0.0, 1.0), child: activeIcon);
     }
+  }
+
+  Widget _getIcon() {
+    final Map<IconType, (IconData, Color)> iconMap = {
+      IconType.rocket: (CupertinoIcons.rocket_fill, CupertinoColors.activeBlue),
+      IconType.heart: (CupertinoIcons.heart_fill, CupertinoColors.systemRed),
+      IconType.gear: (
+        CupertinoIcons.settings_solid,
+        CupertinoColors.systemGrey,
+      ),
+      IconType.star: (CupertinoIcons.star_fill, CupertinoColors.systemYellow),
+    };
+    final config = iconMap[_selectedIcon]!;
+    return Icon(config.$1, size: 100, color: config.$2);
+  }
+
+  Widget _buildPlaybackControls() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: CupertinoButton.filled(
+                  onPressed: () => _controller.forward(),
+                  child: const Text('Start'),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: CupertinoButton(
+                  color: CupertinoColors.systemRed,
+                  onPressed: () => _controller.stop(),
+                  child: const Text('Stop'),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              Expanded(
+                child: CupertinoButton(
+                  color: CupertinoColors.systemIndigo,
+                  onPressed: () => _controller.reverse(),
+                  child: const Text('Reverse'),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: CupertinoButton(
+                  color: CupertinoColors.systemGrey,
+                  onPressed: () => _controller.reset(),
+                  child: const Text('Reset'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
